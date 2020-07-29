c10227e3be1b3c566428097f203ad2efbb18203e
faith okpala <faith@users-MacBook-Air-4.local>
2020-07-29 10:32:44 +0100

    added commemt

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-
+//import User from './newApp.js'
 const getInput = document.querySelector(".landingPage");
 
 let user;
 
 //fetching the data from api
-//this is where the api is being called from "https://swapi.dev/api/people/"
+//i made a call to the api  "https://swapi.dev/api/people/" 
+
+const fetchApi = async (response) =>{
+    return fetch(response)
+    .then(res => res.json())
+    .then(data => {
+        return data;
+    })
+}
+
 const getUser = async () =>{
     const dataUsed = await fetchApi("https://swapi.dev/api/people/");
     
@@ -20,14 +29,9 @@ const getUser = async () =>{
 }
 
 
-const fetchApi = async (response) =>{
-    return fetch(response)
-    .then(res => res.json())
-    .then(data => {
-        return data;
-    })
-}
-//populating the ui
+
+
+//displaying the  image and details of the user on the ui
 const populateUser = (user) => {
     return  `
        <div class = "user-character"   data-id = ${user.id}> 
@@ -35,7 +39,7 @@ const populateUser = (user) => {
            <div class= "name"> ${user.name}</div>
        
        <div class = "user-detail">
-           <div>My name is - ${user.name}</div>
+       <div>My name is - ${user.name}</div>
            <div>I am a - ${user.gender}</div>
            <div>with a height of - ${user.height}</div>
            <div> my age is - ${user.birth_year}</div>
@@ -46,6 +50,9 @@ const populateUser = (user) => {
 }
 
 //adding the click event and removing the event
+//the first thing to do is to target the parameter which in this case is the user character
+//then add an event listener click, the other functions shows or hide the value
+
 const addClick = () => {
      document.querySelectorAll(".user-character").forEach(data=> {
         data.addEventListener("click", displayUser);
@@ -70,11 +77,11 @@ const displayDetails = async(users) => {
         output += populateUser(user);
     }).join("")
     getInput.innerHTML = output;
-    addClick();
 
+    addClick();
 }
 
-//the url for the  images
+//the this is an arrray of images to be added to the display details
 const images = [
     "./image/luke_Skywalker.png",
     "./image/Darth-Vader.jpeg",
